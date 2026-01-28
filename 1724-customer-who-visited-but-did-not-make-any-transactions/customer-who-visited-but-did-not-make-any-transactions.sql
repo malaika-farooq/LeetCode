90da19ee494a0c866d26779a83d20afd3edabd78
she-codes <143275452+malaika-farooq@users.noreply.github.com>
2026-01-27 21:30:28 -0600

    Time: 1468 ms (20.30%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,6 @@
+
+SELECT distinct(v.customer_id), count(*) over (partition by v.customer_id) as count_no_trans from Visits v 
+left join Transactions t on v.visit_id = t.visit_id 
+where t.transaction_id is null 
+
+
