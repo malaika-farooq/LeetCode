5d2c68a80a5bc099e2d7fdb7428a4709f56fc053
she-codes <143275452+malaika-farooq@users.noreply.github.com>
2026-01-27 21:32:16 -0600

    Time: 1340 ms (39.94%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,5 +1,5 @@
 
-SELECT distinct(v.customer_id), count(*) as count_no_trans from Visits v 
+SELECT v.customer_id, count(*) as count_no_trans from Visits v 
 left join Transactions t on v.visit_id = t.visit_id 
 where t.transaction_id is null
 group by v.customer_id
