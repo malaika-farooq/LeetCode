4b85a1ddd814e2642464297de95a857a2cc93e3b
she-codes <143275452+malaika-farooq@users.noreply.github.com>
2026-02-04 20:46:16 -0600

    Time: 556 ms (99.31%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
-SELECT Employee.name, bonus
+SELECT name, bonus
 from Employee
 left join Bonus on Employee.empid =  Bonus.empid 
 where bonus is NULL or bonus < 1000
